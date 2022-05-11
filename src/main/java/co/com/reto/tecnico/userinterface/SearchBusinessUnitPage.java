9909a04a85096250af7f06088420ba406971c484
Cristian Avendaño <cavendanoc@choucairtesting.com>
2022-05-11 13:05:09 -0500

    se crea base para escenario cambio de idioma, adicional se cambio xpath para validar escenario anterior parte 2

--- src/main/java/co/com/reto/tecnico/userinterface/SearchBusinessUnitPage.java
+++ src/main/java/co/com/reto/tecnico/userinterface/SearchBusinessUnitPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 
 public class SearchBusinessUnitPage {
     public static final Target NAME_BUSINESS_UNIT = Target.the("")
-            .located(By.xpath("/html/body/main/section/div/div[3]/div[4]/div[3]/div/div[1]/div[2]"));
+            .located(By.xpath("(//a[@data-item-type='Pro.Organization.BusinessUnit'])[2]"));
 
     public static final Target INPUT_NAME_BUSINESS_UNIT = Target.the("")
             .located(By.cssSelector("input[title='enter the text to search for...']"));
