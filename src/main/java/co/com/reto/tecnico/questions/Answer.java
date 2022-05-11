abd10c7bb5003949cb8657d758a20cd51b919e2b
Cristian Avendaño <cavendanoc@choucairtesting.com>
2022-05-11 08:13:14 -0500

    inicio a mejorar el answer de los escenarios

--- src/main/java/co/com/reto/tecnico/questions/Answer.java
+++ src/main/java/co/com/reto/tecnico/questions/Answer.java
@@ -4,6 +4,7 @@ import co.com.reto.tecnico.model.MeetingModel;
 import co.com.reto.tecnico.userinterface.SearchMeetingPage;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.questions.Text;
 
 import java.util.List;
@@ -21,9 +22,11 @@ public class Answer implements Question<Boolean> {
 
     @Override
     public Boolean answeredBy(Actor actor) {
+        //actor.attemptsTo(Click.on());
+
         String nameQueryMeeting = Text.of(SearchMeetingPage.NAME_MEETING).viewedBy(actor).asString();
-        System.out.println("valor enviado desde datadrive: "+nameMeeting);
-        System.out.println("valor tomado desde page: "+nameQueryMeeting);
+        //System.out.println("valor enviado desde datadrive: "+nameMeeting);
+        //System.out.println("valor tomado desde page: "+nameQueryMeeting);
 
         if(nameMeeting.equals(nameQueryMeeting)){
             return true;
