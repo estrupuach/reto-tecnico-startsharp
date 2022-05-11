50829ac7b5cd39613d5e5f72ce6dede69ab8eb5b
Cristian Camilo Avendaño Cadavid <99198072+estrupuach@users.noreply.github.com>
2022-05-11 13:56:02 -0500

    xpath menos estricto

--- src/main/java/co/com/reto/tecnico/userinterface/SearchMeetingPage.java
+++ src/main/java/co/com/reto/tecnico/userinterface/SearchMeetingPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 
 public class SearchMeetingPage {
     public static final Target NAME_MEETING = Target.the("")
-            .located(By.xpath("//a[text()='MeetingTestt']"));
+            .located(By.xpath("//a[@data-item-type='Pro.Meeting.Meeting'])[2]"));
     public static final Target INPUT_NAME_MEETING = Target.the("")
             .located(By.cssSelector("input[title='enter the text to search for...']"));
 }
